8bd6604e365d472c7df76ce2dd08ff53b92c738d
withimat <imattgokk@gmail.com>
2025-06-30 15:28:45 +0300

    Initial Commit

---
+++ bilgiYarismasi/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  bilgiYarismasi
+//
+//  Created by İmat Gökaslan on 30.06.2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ bilgiYarismasi/bilgiYarismasiApp.swift
@@ -0,0 +1,17 @@
+//
+//  bilgiYarismasiApp.swift
+//  bilgiYarismasi
+//
+//  Created by İmat Gökaslan on 30.06.2025.
+//
+
+import SwiftUI
+
+@main
+struct bilgiYarismasiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
